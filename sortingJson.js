055bea90f4d59a190b6f279b319acdaeec9f5ac0
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-11-02 12:03:59 +0200

    make sorting json
    This code sends requests to the API endpoints, checks the isDone status of the responses, and returns the number of True and False values.

---
+++ sortingJson.js
@@ -0,0 +1,65 @@
+const axios = require("axios");
+
+const allUrl = [
+  "https://jsonbase.com/sls-team/json-793",
+  "https://jsonbase.com/sls-team/json-955",
+  "https://jsonbase.com/sls-team/json-231",
+  "https://jsonbase.com/sls-team/json-931",
+  "https://jsonbase.com/sls-team/json-93",
+  "https://jsonbase.com/sls-team/json-342",
+  "https://jsonbase.com/sls-team/json-770",
+  "https://jsonbase.com/sls-team/json-491",
+  "https://jsonbase.com/sls-team/json-281",
+  "https://jsonbase.com/sls-team/json-718",
+  "https://jsonbase.com/sls-team/json-310",
+  "https://jsonbase.com/sls-team/json-806",
+  "https://jsonbase.com/sls-team/json-469",
+  "https://jsonbase.com/sls-team/json-258",
+  "https://jsonbase.com/sls-team/json-516",
+  "https://jsonbase.com/sls-team/json-79",
+  "https://jsonbase.com/sls-team/json-706",
+  "https://jsonbase.com/sls-team/json-521",
+  "https://jsonbase.com/sls-team/json-350",
+  "https://jsonbase.com/sls-team/json-64",
+];
+
+const RetryUrl = 3;
+
+const fetchUrl = async (url, RetryUrl) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    if (RetryUrl > 0) {
+      return fetchUrl(url, RetryUrl - 1);
+    } else {
+      console.error(`[Fail] ${url}: The endpoint is unavailable`);
+      return null;
+    }
+  }
+};
+
+const sorting = async () => {
+  let countTrue = 0;
+  let countFals = 0;
+
+  for (const url of allUrl) {
+    const data = await fetchUrl(url, RetryUrl);
+
+    if (data && data.hasOwnProperty("isDone")) {
+      const isDone = data.isDone;
+      console.log(`[Success] ${url}: isDone - ${isDone ? "True" : "False"}`);
+
+      if (isDone) {
+        countTrue++;
+      } else {
+        countFals++;
+      }
+    }
+  }
+
+  console.log(`Found True values: ${countTrue}`);
+  console.log(`Found False values: ${countFals}`);
+};
+
+sorting();
